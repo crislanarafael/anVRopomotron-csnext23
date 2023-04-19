cc2bed6ee9c0a27f03158c9bff0e9af09a8f3329
Crislana Rafael <crislanarafael@gmail.com>
2023-04-19 11:03:38 -0600

    Create index.js
    Created sample data structure for location cues

---
+++ js/src/locationcues/index.js
@@ -0,0 +1,26 @@
+var locations = [
+    {
+        //sample location data
+        "x-position" : 4.5,
+        "y-position" : 0,
+        "z-position" : 4,
+        "volume" : [4.5, 0, 4], //requires review
+        "description" : ["Chimp Statue Model holding a branch"]
+    },
+    {
+        //sample location data
+        "x-position" : 4.94,
+        "y-position" : 0,
+        "z-position" : -2.1,
+        "volume" : [4.94, 0, -2.1], //requires review
+        "description" : ["Artifact table labeled 'Artifacts' containing 7 artifacts (left to right): Lomekwian Hammerstone, Acheulian Handaxe, Mousterian Scraper, Clovis Lanceolate Spear Point, Hippopotamus Ivory Harpoon, Engraved Water Bottle, Human Femur Dagger"]
+    },
+    {
+        //sample location data
+        "x-position" : 4.94,
+        "y-position" : 0,
+        "z-position" : 1,
+        "volume" : [4.94, 0, 1], //requires review
+        "description" : ["Artifact table labeled 'Primate Figures' containing 4 artifacts (left to right): Woman of Willendorf, Canopic Jar Lid, Nasca Vessel, Baboon Figurine"]
+    }
+]
