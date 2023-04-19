9bd5e0d7727aee4ab8d17e604fa21b1fb5c92cbe
Crislana Rafael <crislanarafael@gmail.com>
2023-04-19 11:07:46 -0600

    Update index.js
    Added title property

--- js/src/locationcues/index.js
+++ js/src/locationcues/index.js
@@ -1,6 +1,7 @@
 var locations = [
     {
         //sample location data
+        "title" : "Chimp Statue Model",
         "x-position" : 4.5,
         "y-position" : 0,
         "z-position" : 4,
@@ -9,6 +10,7 @@ var locations = [
     },
     {
         //sample location data
+        "title": "Artifact Table",
         "x-position" : 4.94,
         "y-position" : 0,
         "z-position" : -2.1,
@@ -17,10 +19,12 @@ var locations = [
     },
     {
         //sample location data
+        "title" : "Primate Figure Artifact Table",
         "x-position" : 4.94,
         "y-position" : 0,
         "z-position" : 1,
         "volume" : [4.94, 0, 1], //requires review
         "description" : ["Artifact table labeled 'Primate Figures' containing 4 artifacts (left to right): Woman of Willendorf, Canopic Jar Lid, Nasca Vessel, Baboon Figurine"]
-    }
+    },
+    
 ]
