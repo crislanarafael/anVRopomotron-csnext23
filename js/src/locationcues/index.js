c99fd0b4f1ad4ea8126ee03cd1b37f0f5b89e7ab
Crislana Rafael <crislanarafael@gmail.com>
2023-04-21 12:21:09 -0600

    Update index.js
    Updated positioning of locations to be based on rig

--- js/src/locationcues/index.js
+++ js/src/locationcues/index.js
@@ -1,30 +1,84 @@
 var locations = [
     {
-        //sample location data
-        "title" : "Chimp Statue Model",
-        "x-position" : 4.5,
+        //positioning based on rig
+        "title" : "Location Main Hall",
+        "x-position" : 0.115,
         "y-position" : 0,
-        "z-position" : 4,
-        "volume" : [4.5, 0, 4], //requires review
-        "description" : ["Chimp Statue Model holding a branch"]
+        "z-position" : 1.518,
+        "volume" : [0.115, 0, 1.518], //requires review
+        "description" : ["You are in the center of the Main Hall"]
     },
     {
-        //sample location data
-        "title": "Artifact Table",
-        "x-position" : 4.94,
+        "title": "Between Main Hall and Scale Model Hall",
+        "x-position" : -5.104,
         "y-position" : 0,
-        "z-position" : -2.1,
-        "volume" : [4.94, 0, -2.1], //requires review
-        "description" : ["Artifact table labeled 'Artifacts' containing 7 artifacts (left to right): Lomekwian Hammerstone, Acheulian Handaxe, Mousterian Scraper, Clovis Lanceolate Spear Point, Hippopotamus Ivory Harpoon, Engraved Water Bottle, Human Femur Dagger"]
+        "z-position" : 5.185,
+        "volume" : [-5.104, 0, 5.185], //requires review
+        "description" : ["You are between the Main Hall and Scale Model Hall"]
     },
     {
-        //sample location data
-        "title" : "Primate Figure Artifact Table",
-        "x-position" : 4.94,
+        "title" : "Prehistoric Primate Hall",
+        "x-position" : -5.820,
         "y-position" : 0,
-        "z-position" : 1,
-        "volume" : [4.94, 0, 1], //requires review
-        "description" : ["Artifact table labeled 'Primate Figures' containing 4 artifacts (left to right): Woman of Willendorf, Canopic Jar Lid, Nasca Vessel, Baboon Figurine"]
+        "z-position" : -1.998,
+        "volume" : [-5.820, 0, -1.998], //requires review
+        "description" : ["You are in the Prehistoric Primate Hall"]
     },
-    
+    {
+        "title" : "Modern Primate Hall",
+        "x-position" : -5.820,
+        "y-position" : 0,
+        "z-position" : -1.998,
+        "volume" : [-5.820, 0, -1.998], //requires review
+        "description" : ["You are in the middle of the Modern Primate Hall"]
+    },
+    {
+        "title" : "Between Modern Primate Hall and Burial Chamber",
+        "x-position" : -5.820,
+        "y-position" : 0,
+        "z-position" : -1.998,
+        "volume" : [-5.820, 0, -1.998], //requires review
+        "description" : ["You are in between the Modern Primate Hall and Burial Chamber"]
+    },
+    {
+        "title" : "Burial Chamber",
+        "x-position" : -13.741,
+        "y-position" : 0,
+        "z-position" : -15.385,
+        "volume" : [-13.741, 0, -15.385], //requires review
+        "description" : ["You are in the middle of the Burial Chamber"]
+    },
+    {
+        "title" : "Between Prehistoric Primate Hall and Human Evolution Hall",
+        "x-position" : -7.799,
+        "y-position" : 0,
+        "z-position" : -7.573,
+        "volume" : [-7.799, 0, -7.573], //requires review
+        "description" : ["You are in the between the Prehistoric Primate Hall and Human Evolution Hall"]
+    },
+    {
+        "title" : "Human Evolution Hall",
+        "x-position" : -4.582,
+        "y-position" : 0,
+        "z-position" : -13.901,
+        "volume" : [-4.582, 0, -13.901], //requires review
+        "description" : ["You are in the middle of the Human Evolution Hall"]
+    },
+    {
+        "title" : "Between Main Hall and Grab Lab",
+        "x-position" : 7.728,
+        "y-position" : 0,
+        "z-position" : -7.189,
+        "volume" : [7.728, 0, -7.189], //requires review
+        "description" : ["You are in between the Main Hall and Grab Lab"]
+    },
+    {
+        "title" : "VR Grab Lab",
+        "x-position" : 8.809,
+        "y-position" : 0,
+        "z-position" : -0.330,
+        "volume" : [8.809, 0, -0.330], //requires review
+        "description" : ["You are in the middle of the VR Grab Lab"]
+    },
+
 ]
